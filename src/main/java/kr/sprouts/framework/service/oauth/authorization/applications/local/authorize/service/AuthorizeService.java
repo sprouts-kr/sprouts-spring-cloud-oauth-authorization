47059ea2f094c12589d93bd9b0a52799bb5cb2a2
Jinwon Seo <jwseo@live.co.kr>
2023-08-31 08:55:50 +0900

    Remove provider credential api.

--- src/main/java/kr/sprouts/framework/service/oauth/authorization/applications/local/authorize/service/AuthorizeService.java
+++ src/main/java/kr/sprouts/framework/service/oauth/authorization/applications/local/authorize/service/AuthorizeService.java
@@ -2,8 +2,6 @@ package kr.sprouts.framework.service.oauth.authorization.applications.local.auth
 
 import kr.sprouts.framework.autoconfigure.security.credential.consumer.components.BearerTokenCredentialConsumer;
 import kr.sprouts.framework.autoconfigure.security.credential.consumer.components.CredentialConsumerManager;
-import kr.sprouts.framework.autoconfigure.security.credential.provider.components.ApiKeyCredentialProvider;
-import kr.sprouts.framework.autoconfigure.security.credential.provider.components.ApiKeySubject;
 import kr.sprouts.framework.autoconfigure.security.credential.provider.components.BearerTokenCredentialProvider;
 import kr.sprouts.framework.autoconfigure.security.credential.provider.components.BearerTokenSubject;
 import kr.sprouts.framework.autoconfigure.security.credential.provider.components.CredentialProviderManager;
@@ -12,10 +10,8 @@ import kr.sprouts.framework.library.security.credential.Credential;
 import kr.sprouts.framework.library.security.credential.CredentialHeaderSpec;
 import kr.sprouts.framework.library.security.credential.codec.Codec;
 import kr.sprouts.framework.library.security.credential.codec.CodecType;
-import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.dto.proxy.AuthorizationProxy;
 import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.dto.proxy.AuthorizeProxy;
 import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.dto.proxy.BearerTokenProxy;
-import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.dto.proxy.CredentialProxy;
 import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.dto.proxy.MemberProxy;
 import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.exception.AuthorizeRemoteServiceInitializeFailedException;
 import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.exception.UnAuthorizedException;
@@ -173,20 +169,6 @@ public class AuthorizeService {
         return MemberProxy.fromRemoteResponse(memberAtomicReference.get());
     }
 
-    private String apiKey(UUID providerId, UUID memberId) {
-        AtomicReference<ApiKeyCredentialProvider> credentialProviderAtomicReference = new AtomicReference<>();
-
-        credentialProviderManager.getProvider(providerId).ifPresentOrElse(credentialProvider -> {
-            if (Boolean.FALSE.equals(credentialProvider instanceof ApiKeyCredentialProvider)) {
-                throw new RuntimeException("Unsupported credential provider.");
-            }
-
-            credentialProviderAtomicReference.set((ApiKeyCredentialProvider) credentialProvider);
-        }, () -> { throw new RuntimeException("Credential provider not found."); });
-
-        return codec.encodeToString(SerializationUtils.serialize(credentialProviderAtomicReference.get().provide(ApiKeySubject.of(memberId))));
-    }
-
     private String bearerToken(UUID providerId, Long validityInMinutes, UUID memberId) {
         AtomicReference<BearerTokenCredentialProvider> credentialProviderAtomicReference = new AtomicReference<>();
 
@@ -199,53 +181,5 @@ public class AuthorizeService {
         }, () -> { throw new RuntimeException("Credential provider not found."); });
 
         return codec.encodeToString(SerializationUtils.serialize(credentialProviderAtomicReference.get().provide(BearerTokenSubject.of(memberId, validityInMinutes))));
-    }
-
-    public CredentialProxy verification(UUID providerId, String email, String password) {
-        AtomicReference<MemberVerificationRemoteResponse> contentAtomicReference = new AtomicReference<>();
-
-        resourceRemoteClient.verificationMember(email, password).getContent().ifPresentOrElse(content -> {
-            if (Boolean.FALSE.equals(content.isVerification())) throw new UnAuthorizedException();
-
-            contentAtomicReference.set(content);
-        }, UnAuthorizedException::new);
-
-        AtomicReference<MemberRemoteResponse> memberAtomicReference = new AtomicReference<>();
-
-        contentAtomicReference.get().getMember().ifPresentOrElse(member -> {
-            if (member.getId() == null) throw new UnAuthorizedException();
-
-            memberAtomicReference.set(member);
-        }, UnAuthorizedException::new);
-
-        AtomicReference<Credential> credentialAtomicReference = new AtomicReference<>();
-
-        credentialProviderManager.getProvider(providerId).ifPresentOrElse(credentialProvider -> {
-            if (credentialProvider instanceof ApiKeyCredentialProvider) {
-                credentialAtomicReference.set(
-                        ((ApiKeyCredentialProvider) credentialProvider).provide(ApiKeySubject.of(memberAtomicReference.get().getId()))
-                );
-            } else if (credentialProvider instanceof BearerTokenCredentialProvider) {
-                credentialAtomicReference.set(
-                        ((BearerTokenCredentialProvider) credentialProvider).provide(BearerTokenSubject.of(memberAtomicReference.get().getId(), 60L))
-                );
-            } else {
-                throw new UnAuthorizedException();
-            }
-        }, UnAuthorizedException::new);
-
-        if (credentialAtomicReference.get() == null) {
-            throw new UnAuthorizedException();
-        }
-
-        String encodedCredential = codec.encodeToString(SerializationUtils.serialize(credentialAtomicReference.get()));
-
-        String authorizationValue = isEmpty(credentialHeaderSpec.getPrefix()) ?
-                encodedCredential : String.format("%s %s", credentialHeaderSpec.getPrefix(), encodedCredential);
-
-        return CredentialProxy.of(
-                AuthorizationProxy.of(credentialHeaderSpec.getName(), authorizationValue),
-                MemberProxy.fromRemoteResponse(memberAtomicReference.get())
-        );
     }
 }
