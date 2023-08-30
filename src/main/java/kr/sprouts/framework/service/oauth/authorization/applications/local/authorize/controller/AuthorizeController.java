e25867903d06889d67a430e62900e59d0611b579
Jinwon Seo <jwseo@live.co.kr>
2023-08-30 13:42:28 +0900

    Add accessToken api. Closed #16

--- src/main/java/kr/sprouts/framework/service/oauth/authorization/applications/local/authorize/controller/AuthorizeController.java
+++ src/main/java/kr/sprouts/framework/service/oauth/authorization/applications/local/authorize/controller/AuthorizeController.java
@@ -2,7 +2,9 @@ package kr.sprouts.framework.service.oauth.authorization.applications.local.auth
 
 import kr.sprouts.framework.autoconfigure.web.response.components.entity.StructuredResponse;
 import kr.sprouts.framework.autoconfigure.web.response.components.entity.StructuredResponseEntity;
+import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.dto.request.AccessTokenRequest;
 import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.dto.request.CredentialRequest;
+import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.dto.response.AccessTokenResponse;
 import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.dto.response.CredentialResponse;
 import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.service.AuthorizeService;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +29,13 @@ public class AuthorizeController {
     public StructuredResponseEntity credential(@PathVariable(value = "providerId") UUID providerId, @RequestBody CredentialRequest request) {
         return StructuredResponse.succeeded(CredentialResponse.fromProxy(
                 authorizeService.verification(providerId, request.getEmail(), request.getPassword())
+        ));
+    }
+
+    @PostMapping(value = "/access-token")
+    public StructuredResponseEntity accessToken(@RequestBody AccessTokenRequest request) {
+        return StructuredResponse.succeeded(AccessTokenResponse.fromProxy(
+                authorizeService.accessToken(request.getEmail(), request.getPassword())
         ));
     }
 }
--- src/main/java/kr/sprouts/framework/service/oauth/authorization/applications/local/authorize/service/AuthorizeService.java
+++ src/main/java/kr/sprouts/framework/service/oauth/authorization/applications/local/authorize/service/AuthorizeService.java
@@ -10,6 +10,7 @@ import kr.sprouts.framework.library.security.credential.Credential;
 import kr.sprouts.framework.library.security.credential.CredentialHeaderSpec;
 import kr.sprouts.framework.library.security.credential.codec.Codec;
 import kr.sprouts.framework.library.security.credential.codec.CodecType;
+import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.dto.proxy.AccessTokenProxy;
 import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.dto.proxy.AuthorizationProxy;
 import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.dto.proxy.CredentialProxy;
 import kr.sprouts.framework.service.oauth.authorization.applications.local.authorize.dto.proxy.MemberProxy;
@@ -62,6 +63,20 @@ public class AuthorizeService {
         if (this.codec == null) {
             throw new AuthorizeRemoteServiceInitializeFailedException();
         }
+
+        if (accessTokenProviderId == null || accessTokenValidityInMinutes == null || refreshTokenProviderId == null || refreshTokenValidityInMinutes == null) {
+            throw new AuthorizeRemoteServiceInitializeFailedException();
+        }
+
+        this.accessTokenProviderId = accessTokenProviderId;
+        this.accessTokenValidityInMinutes = accessTokenValidityInMinutes;
+        this.refreshTokenProviderId = refreshTokenProviderId;
+        this.refreshTokenValidityInMinutes = refreshTokenValidityInMinutes;
+
+        if (log.isInfoEnabled()) {
+            log.info("Initialized access token provider '{}' {} minutes", this.accessTokenProviderId, this.accessTokenValidityInMinutes);
+            log.info("Initialized refresh token provider '{}' {} minutes", this.refreshTokenProviderId, this.refreshTokenValidityInMinutes);
+        }
     }
 
     public CredentialProxy verification(UUID providerId, String email, String password) {
@@ -110,5 +125,67 @@ public class AuthorizeService {
                 AuthorizationProxy.of(credentialHeaderSpec.getName(), authorizationValue),
                 MemberProxy.fromRemoteResponse(memberAtomicReference.get())
         );
+    }
+
+    public AccessTokenProxy accessToken(String email, String password) {
+        MemberRemoteResponse memberRemoteResponse = verification(email, password);
+
+        String encodedCredential = bearerToken(accessTokenProviderId, accessTokenValidityInMinutes, memberRemoteResponse.getId());
+
+        String authorizationValue = isEmpty(credentialHeaderSpec.getPrefix()) ?
+                encodedCredential : String.format("%s %s", credentialHeaderSpec.getPrefix(), encodedCredential);
+
+        return AccessTokenProxy.of(
+                AuthorizationProxy.of(credentialHeaderSpec.getName(), authorizationValue),
+                MemberProxy.fromRemoteResponse(memberRemoteResponse)
+        );
+    }
+
+    private MemberRemoteResponse verification(String email, String password) {
+        AtomicReference<MemberVerificationRemoteResponse> contentAtomicReference = new AtomicReference<>();
+
+        resourceRemoteClient.verification(email, password).getContent().ifPresentOrElse(content -> {
+            if (Boolean.FALSE.equals(content.isVerification())) throw new UnAuthorizedException();
+
+            contentAtomicReference.set(content);
+        }, UnAuthorizedException::new);
+
+        AtomicReference<MemberRemoteResponse> memberAtomicReference = new AtomicReference<>();
+
+        contentAtomicReference.get().getMember().ifPresentOrElse(member -> {
+            if (member.getId() == null) throw new UnAuthorizedException();
+
+            memberAtomicReference.set(member);
+        }, UnAuthorizedException::new);
+
+        return memberAtomicReference.get();
+    }
+
+    private String apiKey(UUID providerId, UUID memberId) {
+        AtomicReference<ApiKeyCredentialProvider> credentialProviderAtomicReference = new AtomicReference<>();
+
+        credentialProviderManager.getProvider(providerId).ifPresentOrElse(credentialProvider -> {
+            if (Boolean.FALSE.equals(credentialProvider instanceof ApiKeyCredentialProvider)) {
+                throw new RuntimeException("Unsupported credential provider.");
+            }
+
+            credentialProviderAtomicReference.set((ApiKeyCredentialProvider) credentialProvider);
+        }, () -> { throw new RuntimeException("Credential provider not found."); });
+
+        return codec.encodeToString(SerializationUtils.serialize(credentialProviderAtomicReference.get().provide(ApiKeySubject.of(memberId))));
+    }
+
+    private String bearerToken(UUID providerId, Long validityInMinutes, UUID memberId) {
+        AtomicReference<BearerTokenCredentialProvider> credentialProviderAtomicReference = new AtomicReference<>();
+
+        credentialProviderManager.getProvider(providerId).ifPresentOrElse(credentialProvider -> {
+            if (Boolean.FALSE.equals(credentialProvider instanceof BearerTokenCredentialProvider)) {
+                throw new RuntimeException("Unsupported credential provider.");
+            }
+
+            credentialProviderAtomicReference.set((BearerTokenCredentialProvider) credentialProvider);
+        }, () -> { throw new RuntimeException("Credential provider not found."); });
+
+        return codec.encodeToString(SerializationUtils.serialize(credentialProviderAtomicReference.get().provide(BearerTokenSubject.of(memberId, validityInMinutes))));
     }
 }
